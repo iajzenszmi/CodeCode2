d7e47771bbeafc2566ba1948faaf03b9764b1e39
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2022-03-04 07:09:24 +1100

    Add files via upload

---
+++ cassert.c
@@ -0,0 +1,13 @@
+ // Programmed and run by Ian Martin Ajzenszmidt, Melbourne Australia.  
+    #include <assert.h>
+    
+      int main()
+       {
+               int    True = 1;
+               int    False = 0;
+	       int    IsSane;
+	       IsSane = True;
+               assert (IsSane == False);
+               	      
+       }
+      
---
+++ cassertprt.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int main(){
+printf("\n%s\n","Programmed and run by Ian Martin Ajzenszmidt, Melbourne, Australia. ");
+}
