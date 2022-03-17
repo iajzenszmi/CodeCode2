7a9fc1da7cc071fac5f6b61390e90caa8f27042e
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2022-03-17 19:18:19 +1100

    Add files via upload

---
+++ sinelist.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+     int i;
+     printf("\n%s","table of numbers and sines");
+     printf("\n%s","number                sine");
+     for(i=1;i<=10;i++){
+     printf("\n%d%s%f",i,"                   ",sin(i));
+     }
+     }
+     
