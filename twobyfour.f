8edf47eddb66a39fbb88c1a21231b30e40c54a99
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2022-03-24 09:45:48 +1100

    Add files via upload

---
+++ twobyfour.f
@@ -0,0 +1,8 @@
+       integer two
+       integer four
+       integer result1
+       two = 2
+       four = 4
+       result1 = two * four
+       print *, two, four, result1
+       end program    
