b4f70a0b582a9c3421a6781060039bcb897a10b6
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2022-04-19 17:11:55 +1000

    Add files via upload

---
+++ runcount.f
@@ -0,0 +1,6 @@
+            integer rcount
+            do rcount = 1, 10, 1
+              write(6,9000) rcount
+ 9000       format(" "," rcount = ", i10)
+            end do
+            end program            
