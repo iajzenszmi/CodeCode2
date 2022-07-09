9ebb91692e1cfc27ad532af3b698074681d10c8b
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2022-07-10 00:22:56 +1000

    Add files via upload

---
+++ struct.py
@@ -0,0 +1,21 @@
+def add_two_and_two(): 
+    return 2 + 2
+
+def coin_toss_randomly():
+    import random as ra
+    import numpy as np
+    return ra.choice(['heads', 'tails'])                                                         
+                                                                                                                                    
+                                                                                                                      
+                                                                                                                                                
+                   
+
+print(add_two_and_two())
+for i in range(10):
+   print(coin_toss_randomly())
+
+
+
+
+
+
