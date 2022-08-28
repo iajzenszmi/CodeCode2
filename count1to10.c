acc065e8367465695a98cc44794a63a1d4c63562
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2022-08-28 19:53:42 +1000

    Add files via upload

--- count1to10.c
+++ count1to10.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
-int main(){
-   int count1;
-   for(count1=1;count1<=10;count1++){
-	    printf("%d\n",count1);
-	   }
-	   return 0;
+// program to count from   1 to 10
+// Author: Ian Martin Ajzenszmidt
+//
+// Compile: gcc count1to10.c -o count1to10
+//Run: ./count1to10
 
-   }
+#include <stdio.h> // include the standard input output library
 
-
+int main() { // main function
+        int i; // declare i as an integer                                                                                                     
+        for (i = 1; i <= 10; i++) { // for loop to count from 1 to 10                                                                         
+                printf("%d\n", i); // print the value of i                                                                                    
+        }                                                                                                                                     
+        return 0; // return 0                                                                                                                 
+} // end of main function
