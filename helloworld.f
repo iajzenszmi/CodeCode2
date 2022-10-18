81f7923e20402ce4d5b818e0203686e45591fba2
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2022-10-19 08:49:16 +1100

    Add files via upload

---
+++ helloworld.f
@@ -0,0 +1,8 @@
+       character(len=6) hello
+       character(len=6) world
+       character(len=12) helloworld
+       hello = "hello "
+       world = " world"
+       helloworld = hello // world
+       print *,helloworld
+       end program
