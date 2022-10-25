cf5c60d8399b42d1c5b17547fd18e6ed13e96f92
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2022-10-25 16:08:39 +1100

    Add files via upload

---
+++ precision.f90
@@ -0,0 +1,14 @@
+          program precision
+             implicit none
+
+            ! Declare
+
+            double precision :: x,y,z
+
+           !Main
+          y=10.0
+          x = 3.0
+           z=y/x 
+           print *,z
+           end program
+
