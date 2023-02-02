a46f98c0036562af72af799c6b43b98cd9a7d84c
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2023-02-02 23:08:26 +1100

    Add files via upload

---
+++ poww.java
@@ -0,0 +1,13 @@
+public class poww
+{
+public static void main(String[] args)
+ {
+int power = 1;
+for(int i = 0;i <=10;i++)
+{
+  System.out.println(i + " " + power);
+  power = 2*power;
+  }
+  }
+  }
+  
