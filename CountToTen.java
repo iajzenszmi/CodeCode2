c815f5e059e004c3c31d6209bbdcd0164de91fa6
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2023-04-02 04:14:00 +1000

    Add files via upload

---
+++ CountToTen.java
@@ -0,0 +1,14 @@
+class CountToTen {
+    public static void main(String args[]) {
+	    int counta;
+	    for(counta=1;counta < 11;counta++){
+		    if(counta <= 9 ){
+		    System.out.printf("\nCount = %d ", counta);
+	    }
+	            if(counta == 10){
+		      System.out.printf("\nCount = %d\n", counta);
+		    }	      
+    }       
+    }
+}
+
