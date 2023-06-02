ad4847eba458035b80862f6ce8169454fb3cc251
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2023-06-02 14:27:31 +1000

    Add files via upload

---
+++ alvinpurple.f
@@ -0,0 +1,7 @@
+              integer n, i
+        print *,"How many times do you want me to output Alvin Purple"
+              read *,n
+              do i = 1,n
+              print *,"Alvin Purple"
+              end do
+              end program
